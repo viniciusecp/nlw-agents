076adef80b71638c04f3ba24eafa6048bcdfcce1
Vinicius Faustino Silva <3sfvinicius@ache.com.br>
2025-07-11 17:02:04 -0300

    Initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import fastifyCors from '@fastify/cors';
+import { fastify } from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { env } from './env.ts';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: '*',
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get('/health', () => {
+  return { status: 'ok' };
+});
+
+app.listen({ port: env.PORT });
